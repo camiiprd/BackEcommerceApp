8e7e73c3c6fc4cbfe44100386b3ba06db7020cde
camiiprd <camila.prado.031@gmail.com>
2024-08-05 04:13:12 -0300

    delete .env

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
 import { createPool } from "mysql2/promise";
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 
-dotenv.config();
+//dotenv.config();
 
 export const pool = createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: "localhost",
+    user: "root",
+    password: "3129",
+    database: "ecommercedbpruebav2",
 });
 
 pool.getConnection((error, connection) => {
